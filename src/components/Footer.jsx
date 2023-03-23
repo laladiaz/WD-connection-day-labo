3407b35b9f9a6b41a167e2a748a3deefe368f782
laladiaz <mariaabelardadiaz@gmail.com>
2023-03-23 13:27:59 -0600

    adds the footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export const Footer = () =>{
+    return(
+        <footer>
+            <div>
+                <p>
+                © 2023 Webquipo. 
+                </p>
+            </div>
+        </footer>
+    )
+}
